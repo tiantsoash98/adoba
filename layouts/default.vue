c73a5198ac08e611a6d915afe43339cb3aac55cc
tiantsoash98 <tiantsoa.sh@gmail.com>
2024-01-05 19:01:59 +0300

    Init css

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <header class="header">
+            Header here
+        </header>
+
+        <!-- output the page content -->
+        <main class="page-wrapper">
+            <slot />
+        </main>
+
+        <footer class="footer">
+            Footer here
+        </footer>
+    </div>
+</template>
+
+<!-- <style scoped>
+    .router-link-exact-active {
+        color: green;
+    }
+</style> -->
