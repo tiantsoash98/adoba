abf9e820c5f9b5d13061d0231fac7478d9dbbabf
tiantsoash98 <tiantsoa.sh@gmail.com>
2024-01-11 11:50:28 +0300

    Update RealisationsFilterModal.vue

--- components/RealisationsFilterModal.vue
+++ components/RealisationsFilterModal.vue
@@ -8,8 +8,8 @@
         <div class="realisations-modal__backdrop" @click="closeMenu"></div>
         <div class="realisations-modal__wrapper">
             <div class="realisations-modal__top-wrapper">
-                <div class="realisations-modal__icon-wrapper" @click="closeMenu">
-                    <svg class="realisations-modal__icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24.7 24.7">
+                <div class="realisations-modal__icon-wrapper " @click="closeMenu">
+                    <svg class="realisations-modal__icon realisations-modal__close-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24.7 24.7">
                         <polygon points="22.4 24.7 12.4 14.7 2.4 24.7 0 22.4 10 12.4 0 2.4 2.4 0 12.4 10 22.4 0 24.7 2.4 14.7 12.4 24.7 22.4 22.4 24.7"/>
                     </svg>
                 </div>
@@ -115,7 +115,7 @@
             .to('.realisations-modal__wrapper', { 
                 xPercent: -100,
                 duration: 0.6,
-                delay: 0.3
+                delay: 0.2
             })
             .to('.realisations-modal__backdrop', { 
                 opacity: 0,      
@@ -224,6 +224,13 @@
         align-items: center;
         max-width: var(--r-space-md);
         cursor: pointer;
+    }
+    &__close-icon {
+        transition: transform 0.3s var(--alias-default-ease);
+
+        &:hover {
+            transform: rotate(90deg);
+        }
     }
     &__icon{
         width: 100%;
