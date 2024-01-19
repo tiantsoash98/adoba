70709236d73b2e31f5290553cb6e6b34d885fcdf
tiantsoash98 <tiantsoa.sh@gmail.com>
2024-01-19 10:35:46 +0300

    Update Info.vue

--- components/Service/Info.vue
+++ components/Service/Info.vue
@@ -1,14 +1,14 @@
 <template>
     <div>
-        <section class="section service-info">
+        <section class="section service-info" ref="sectionEl">
             <div class="container">
                 <div class="service-info__top-wrapper">
                     <div class="service-info__headline-wrapper">
                         <h2 class="text-visually-hidden">{{ content.serviceInfo.serviceInfoHeadline }}</h2>
-                        <div class="service-info__headline title-h2">{{ content.serviceInfo.serviceInfoHeadline }}</div>
+                        <div class="service-info__headline title-h2" ref="textEl">{{ content.serviceInfo.serviceInfoHeadline }}</div>
                     </div>
                     <div class="service-info__icon-wrapper">
-                        <IconAsterisk icon-class="service-info__icon"></IconAsterisk>
+                        <IconAsterisk :icon-class="{'service-info__icon': true, 'spin-clockwise': isScrollingDown}"></IconAsterisk>
                     </div>
                     <div class="service-info__border-bottom"></div>
                 </div>
@@ -27,9 +27,84 @@
 </template>
 
 <script setup>
+const sectionEl = ref(null)
+const textEl = ref(null)
+const isScrollingDown = ref(false)
+const currentScrollPosition = ref(0)
+const { gsap } = useGsap()
+import SplitType from 'split-type';
 const props = defineProps({
     content: Object
 })
+
+onMounted(() => {
+    animateOnEnter()
+    pageScroll(sectionEl, textEl)
+    window.addEventListener("scroll", onScroll)
+})
+
+onBeforeUnmount(() => {
+    window.removeEventListener("scroll", onScroll)
+})
+
+function onScroll(){
+    isScrollingDown.value = (currentScrollPosition.value < window.scrollY);
+    currentScrollPosition.value = window.scrollY;
+}
+
+const animateOnEnter = () => {
+    let triggerEl = '.service-info';
+    let targetEl = '.hero-img';
+
+    gsap.timeline({
+        defaults: { duration: 1 },
+        scrollTrigger: {
+            trigger: triggerEl,
+            //trigger element - viewport
+            start: "top bottom",
+            end: "top top",
+            scrub: true
+        }
+    })
+    .to(targetEl, {
+        yPercent: 25,
+        ease: "none"
+    })
+}
+
+const pageScroll = (triggerEl, textReveal) => {
+    SplitType.create(textReveal.value, 
+    {
+        types: 'lines', 
+        lineClass: 'text-reveal--line-wrapper'
+    })
+
+    document.querySelectorAll('.text-reveal--line-wrapper')
+        .forEach(function(line){
+            var wrapperDiv = document.createElement('div');
+            wrapperDiv.classList.add('text-reveal--line')
+            wrapperDiv.innerHTML = line.innerHTML
+            line.innerHTML = ""
+            line.appendChild(wrapperDiv)
+        })
+
+    gsap.timeline({
+        defaults: { duration: 1, ease: "power3.out" },
+        scrollTrigger: {
+            trigger: triggerEl.value,
+            //trigger element - viewport
+            start: "top 80%",
+            end: "top center"
+        }
+    })
+    .from('.text-reveal--line', 
+    {
+        opacity: 0,
+        yPercent: 100,
+        stagger: 0.1,
+        delay: 0.4
+    })
+}
 </script>
 
 <style lang="scss" scoped>
@@ -69,6 +144,35 @@ const props = defineProps({
     &__text-wrapper {
         grid-column: 9/span 4;
         margin-top: auto;
+    }
+    &__icon {
+        transition: transform 0.5s linear;
+        animation: spin-counter-clockwise 4s linear infinite;
+    }
+
+    @keyframes spin-clockwise {
+        0% {
+            transform: rotate(0);
+        }
+        100% {
+            transform: rotate(360deg);
+        }
+    }
+    @keyframes spin-counter-clockwise {
+        0% {
+            transform: rotate(0);
+        }
+        100% {
+            transform: rotate(-360deg);
+        }
+    }
+}
+.spin-clockwise {
+    animation: spin-clockwise 4s linear infinite;
+}
+.text-reveal {
+    &--line-wrapper {
+        overflow: hidden;
     }
 }
 </style>
