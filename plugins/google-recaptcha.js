8cbab97d654b078aaa12b419ecf54b8d1ee3754a
tiantsoash98 <tiantsoa.sh@gmail.com>
2024-01-29 15:08:32 +0300

    Autohide recaptcha

--- plugins/google-recaptcha.js
+++ plugins/google-recaptcha.js
@@ -6,7 +6,7 @@ export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.vueApp.use(VueReCaptcha, {
         siteKey: config.public.siteKey,
         loaderOptions: {
-            autoHideBadge: false,
+            autoHideBadge: true,
             explicitRenderParameters: {
                 badge: 'bottomleft',
             },
